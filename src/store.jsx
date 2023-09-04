7f2abb9b6c635cfe9b77628aaa6ef8aaee716b9f
ishant <ishantyadav1996@gmail.com>
2023-09-04 10:09:25 +0530

    Initial commit

---
+++ src/store.jsx
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import rootReducer from "./redux/reducers/main";
+
+
+const store = configureStore({
+    reducer: rootReducer,
+}
+
+);
+
+
+export default store;
